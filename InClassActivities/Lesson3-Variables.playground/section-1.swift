6f1ce58845faa5f2c04e5bf013155eb2d84ac56b
Dustin Pfannenstiel <dustin@nth-estate.com>
2015-03-17 16:53:44 -0400

    added variable playground

---
+++ InClassActivities/Lesson3-Variables.playground/section-1.swift
@@ -0,0 +1,21 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// TODO:  Create a constant called ``name" that stores your name as a string
+
+// TODO:  Create a variable called ``year" to store the current year as an integer
+
+// TODO:  Create a constant ``oscarsBirthYear" as a float and set it to 2011.
+
+// TODO:  Create an optional variable ``oscarsCalendarAge"
+
+// TODO:  Assign oscarsCalendarAge based on his birth year and the current year
+
+// TODO:  Calculate and store oscarsAgeInDog (calendar age * 7)
+
+// TODO:  Print out a statment "I am {your name here}, I met Oscar when he was {oscars age in dog years} in {current year}"
+
+// BONUS:  What happens if you add 2 strings?
+
+// BONUS:  What happens if you add an Int to a string?
