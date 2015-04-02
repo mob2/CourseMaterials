3c9acc493e562bef79e9a822040d007c51843ca5
Dustin Pfannenstiel <dustin@nth-estate.com>
2015-04-02 17:17:47 -0400

    initial in class project

---
+++ InClassActivities/Players/Players/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Players
+//
+//  Created by Dustin Pfannenstiel on 4/2/15.
+//  Copyright (c) 2015 Dustin Pfannenstiel. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+	@IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+	var detailItem: AnyObject? {
+		didSet {
+		    // Update the view.
+		    self.configureView()
+		}
+	}
+
+	func configureView() {
+		// Update the user interface for the detail item.
+		if let detail: AnyObject = self.detailItem {
+		    if let label = self.detailDescriptionLabel {
+		        label.text = detail.description
+		    }
+		}
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		self.configureView()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
